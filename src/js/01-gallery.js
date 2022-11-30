220786031cd15233737aa20d3a8584acefdf52db
Anton Bobryshev <101340267+BobryshevAnton@users.noreply.github.com>
2022-11-30 13:01:22 +0100

    home work 8 independent work

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -2,4 +2,24 @@
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
-console.log(galleryItems);
+// console.log(galleryItems);
+
+// Дополнительный импорт стилей
+import SimpleLightbox from 'simplelightbox';
+
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
+const ulList = document.querySelector('.gallery');
+
+const itemElement = galleryItems.map(({ preview, original, description }) => {
+  return `<a class="gallery__item" href="${original}">
+  <img class="gallery__image" src="${preview}" alt="${description}" />
+</a>`;
+});
+
+ulList.insertAdjacentHTML('afterbegin', itemElement.join(''));
+
+const lightbox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,42 @@
+// import throttle from 'lodash.throttle';
+//refs
+const form = document.querySelector('.feedback-form');
+const inputEmail = document.querySelector('[name="email"]');
+const inputMessage = document.querySelector('[name="message"]');
+const button = document.querySelector('button');
+const LOCAL_KEY = 'feedback-form-state';
 
+//addlistener
+form.addEventListener('input', handleInput);
+form.addEventListener('submit', handleSubmit);
+form.addEventListener('input', populateTextAria);
+
+populateTextAria();
+const userData = { email: inputEmail.value, message: inputMessage.value };
+
+function handleInput(event) {
+  const userData = { email: inputEmail.value, message: inputMessage.value };
+
+  localStorage.setItem(LOCAL_KEY, JSON.stringify(userData));
+}
+
+function handleSubmit(event) {
+  event.preventDefault();
+  if (inputEmail.value === '' || inputMessage.value === '') {
+    alert('Please fill in all the fields!');
+  }
+  localStorage.clear();
+
+  console.log(userData);
+
+  form.reset();
+}
+
+function populateTextAria(event) {
+  const messageExport = localStorage.getItem(LOCAL_KEY);
+  const messageParse = JSON.parse(messageExport);
+  if (messageExport) {
+    inputEmail.value = messageParse.email;
+    inputMessage.value = messageParse.message;
+  }
+}
